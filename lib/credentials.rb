f44f2e051ccee9e931f33717e57e115d72eae344
Luciano Adonis <luciano.adonis@dxc.com>
2020-08-26 10:13:02 -0400

    Add 'bot' to slack's variables

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -1,9 +1,9 @@
 
 class Credentials
   def set_credentials
-    @bot_token = ENV['SLACK_TOKEN']
-    @bot_name = ENV['SLACK_NAME']
-    @bot_icon = ENV['SLACK_ICON']
+    @bot_token = ENV['SLACK_BOT_TOKEN']
+    @bot_name = ENV['SLACK_BOT_NAME']
+    @bot_icon = ENV['SLACK_BOT_ICON']
     @bot_channel = ENV['SLACK_DEFAULT_CHANNEL']
     @bot_default_output = ENV['SLACK_DEFAULT_OUTPUT']
   end
